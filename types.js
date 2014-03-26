b924d2e4737d6a158880af41c0ed7bf3312a6424
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-03-26 16:24:07 +0100

    Delete old subscription.

--- types.js
+++ types.js
@@ -2,27 +2,6 @@ var Type        = require('couchtypes/types').Type;
 var fields      = require('couchtypes/fields');
 var permissions = require('couchtypes/permissions');
 
-var idValidator = function(doc, value) {
-  var _id = doc._id.split('--');
-  if (_id[1] !== doc.object_key || _id[2] !== doc.subscriber) {
-    throw new Error('Incorrect id');
-  }
-}
-
-exports.subscription = new Type('subscription', {
-  permissions: {
-    add: permissions.loggedIn(),
-    update: permissions.usernameMatchesField('subscriber'),
-    remove: permissions.usernameMatchesField('subscriber'),
-  },
-  fields: {
-    object_key: fields.string({
-      validators: [idValidator]
-    }),
-    subscriber: fields.creator(),
-  }
-});
-
 /*
  * basculer en validate.js
 exports.notification = new Type('notification', {
--- views.js
+++ views.js
@@ -1,12 +1,3 @@
-
-exports.subscription_by_object_key = {
-  map: function(doc) {
-    if(doc.type && doc.type == 'subscription') {
-      emit(doc.object_key, {subscriber: doc.subscriber});
-    }
-  }
-};
-
 exports.notification_all = {
   map: function(doc) {
     if(doc.type && doc.type == 'notification') {
