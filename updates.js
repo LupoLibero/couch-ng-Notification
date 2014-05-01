0ae844301a5ad45e533f6be1384098edddf1ff34
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-05-01 15:14:13 +0200

    Add update.

--- updates.js
+++ updates.js
@@ -9,3 +9,26 @@ exports.notification_displayed = function(doc, req) {
   }
 }
 
+exports.notification_send = function(doc, req) {
+  var attr;
+  var form = JSON.parse(req.body);
+  if(doc === null){
+    throw({forbidden: 'Not for creation'});
+  } else {
+    doc.email_sent = true
+    return ([doc, 'ok']);
+  }
+}
+
+exports.notification_create = function(doc, req) {
+  var form = JSON.parse(req.body);
+  if(doc===null){
+    form._id        = form.id
+    form.created_at = new Date().getTime();
+    form.displayed  = false;
+    form.email_sent = false;
+    return ([form, 'ok']);
+  } else {
+    throw({forbidden: 'Not for update'});
+  }
+}
